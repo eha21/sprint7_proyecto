c233fb34845b9dfd11711b6df576ec34fcc81b5c
eha21 <enrique.hernandez.arratia@gmail.com>
2026-09-16 15:48:42 -0600

    version inicial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sprint7_proyecto
 sprint7_proyecto
+
+Proyecto que muestra diferentes gráficos para algunos parámetros del auto
---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import plotly as plt
+import plotly.graph_objects as go
+from pathlib import Path
+
+# Leer los datos del archivo CSV
+car_data = pd.read_csv("vehicles_us.csv")
+
+# Crear un histograma utilizando plotly.graph_objects
+# Se crea una figura vacía y luego se añade un rastro de histograma
+fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+# Opcional: Puedes añadir un título al gráfico si lo deseas
+fig.update_layout(title_text='Distribución del Odómetro')
+
+# Mostrar el gráfico Plotly
+fig.show()
+
+
+# Leer los datos del archivo CSV
+car_data = pd.read_csv("vehicles_us.csv")
+
+# Crear un scatter plot utilizando plotly.graph_objects
+# Se crea una figura vacía y luego se añade un rastro de scatter
+fig = go.Figure(data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+# Opcional: Puedes añadir un título al gráfico si lo deseas
+fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+# Mostrar el gráfico Plotly
+fig.show()
